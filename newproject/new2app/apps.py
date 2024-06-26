cf4e02ed6159b55c882b8b00eb61abc1a6bf9c00
surya gurjar <suryagurjar781@gmail.com>
2024-06-26 18:32:57 +0530

    second commit

---
+++ newproject/new2app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class New2AppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'new2app'
---
+++ newproject/new2app/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from new2app import views
+
+
+
+urlpatterns = [
+    path('home2/',views.home)
+    
+    
+]
